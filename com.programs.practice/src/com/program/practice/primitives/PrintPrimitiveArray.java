56b8bec28fa02ad1ee2e049e3bfd9e36ad54566b
saswatPujari <skpujari08@gmail.com>
2018-04-12 20:03:04 +0530

    Added diagonal string & primitive print program

---
+++ com.programs.practice/src/com/program/practice/primitives/PrintPrimitiveArray.java
@@ -0,0 +1,17 @@
+package com.program.practice.primitives;
+
+// Test and verify the output of an array that contains objects of primitive data type
+public class PrintPrimitiveArray {
+
+	public static void main(String[] args) {
+		
+		int[] intArray = {1,2,3,4,5,6,7,8,9};
+		char[] charArray = {'a','b','c','d','e','f','g'};
+		double[] dblArray = {1.2,2.3,3.4,4.5,5.6};
+		
+		System.out.println(intArray);
+		System.out.println(charArray);
+		System.out.println(dblArray);
+	}
+
+}
---
+++ com.programs.practice/src/com/program/practice/string/DiagonalName.java
@@ -0,0 +1,18 @@
+package com.program.practice.string;
+
+// Write a program to print a name diagonally
+public class DiagonalName {
+
+	public static void main(String[] args) {
+		String name="saswat";
+		for(int i=0;i<name.length();i++) {
+			char[] localArr=new char[i];
+			System.out.print(localArr);
+			System.out.print(name.charAt(i));
+			System.out.println();
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
