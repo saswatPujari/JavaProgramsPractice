b694e78f31ccc6f8bcac0a0ee6036fe34876ef1a
saswatPujari <skpujari08@gmail.com>
2018-04-12 20:12:24 +0530

    Primitive addition class

---
+++ com.programs.practice/src/com/program/practice/primitives/PrimitiveAddition.java
@@ -0,0 +1,15 @@
+package com.program.practice.primitives;
+
+public class PrimitiveAddition {
+
+	public static void main(String[] args) {
+		int a = 10;
+		char c = 'A';
+		double d= 0.05;
+		System.out.println(a+c); // adds a + ascii value of c
+		System.out.println(a+d);
+		
+
+	}
+
+}
