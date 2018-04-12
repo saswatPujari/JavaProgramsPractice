aee45fb7d77113a6784f087db45c2fd7df0377c3
saswatPujari <skpujari08@gmail.com>
2018-04-12 20:29:07 +0530

    Swap two numbers without using third variables

--- com.programs.practice/src/com/program/practice/math/swapNumWOThirdVar.java
+++ com.programs.practice/src/com/program/practice/math/swapNumWOThirdVar.java
@@ -1,9 +1,19 @@
 package com.program.practice.math;
 
+// Swap two numbers without using third variables
 public class swapNumWOThirdVar {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int x =10;
+		int y = 30;
+		
+		x=x+y;
+		y=x-y;
+		x=x-y;
+		
+		System.out.println("x ="+x);
+		System.out.println("y ="+y);
+		
 
 	}
 
