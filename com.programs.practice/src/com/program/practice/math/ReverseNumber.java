1125d2fdb425c7079f1332e183d3f35ae9f094b1
saswatPujari <skpujari08@gmail.com>
2018-04-12 20:39:22 +0530

    Reverse number program update

--- com.programs.practice/src/com/program/practice/math/ReverseNumber.java
+++ com.programs.practice/src/com/program/practice/math/ReverseNumber.java
@@ -1,9 +1,18 @@
 package com.program.practice.math;
 
+// write a java program to reverse an integer
 public class ReverseNumber {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int num =123;
+		int rev=0;
+		while(num !=0) {
+			rev = (num/10)+(num%10);
+			num=num/10;
+		}
+		
+		System.out.println(rev);
 
 	}
 
